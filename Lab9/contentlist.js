85b0e0d28e4530d361a25a05e789d202a5be0595
Connor O'Donnell <connor.odonnell@hotmail.com>
2018-03-05 15:41:49 +0000

    Task 1 completed

--- Lab9/contentlist.js
+++ Lab9/contentlist.js
@@ -1,17 +1,18 @@
 $(function(){
 	// 'main' function called when page is loaded and ready
-	
+
 	// complete the following using jQuery
-	
+
 	// 1. find the addcontent button in task-two.html
 	// 2. add an event listener for 'click' events
 	// 3. assign the addContent() function to the click event
+	$("#addcontent").click(addContent);
 });
 
 function addContent () {
 	// add a list of items to the content div
 	var items = ["hewey", "dewey", "louie"];
-	
+	alert("Jquery is shite, mon then");
 	// build the html string for a <ul> list
 	var items_html = "<ul>";
 	for (var i=0; i < items.length; i++) {
@@ -19,10 +20,9 @@ function addContent () {
 		items_html += "<li>" + item + "</li>";
 	};
 	items_html += "</ul>";
-	
+
 	// using jQuery
 	// 1. find the content div
 	// 2. modify its html attribute by adding items_html
-
-}
-
+	$("#content").html(items_html);
+};
