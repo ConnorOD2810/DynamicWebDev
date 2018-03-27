c17bf258c9ab5edce909efe34dea1564a70a30d6
Connor O'Donnell <connor.odonnell@hotmail.com>
2018-03-27 15:30:19 +0100

    wdwdqa

--- lab13/server.js
+++ lab13/server.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var app = express();
+var knockknock = require('knock-knock-jokes');
+
 app.get('/', function(req, res){
  res.send("Hello world! by express");
 });
 
 app.get('/test', function(req, res){
+//Sets the route^
   res.send("this is route 2");
+  //Sends text to page^
 });
+
+app.get('/route3', function(req, res){
+  res.send("Third route");
+});
+
+app.get('/joke', function(req, res){
+  var joke = knockknock();
+  res.end(joke);
+});
+
 app.listen(8080);
